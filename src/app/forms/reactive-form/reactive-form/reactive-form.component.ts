53db608ff0a2cd6e93ca11ae9a4da74cbf09809f
Savitri <savitri26m@gmail.com>
2020-06-08 01:08:59 +0530

    added async validator

--- src/app/forms/reactive-form/reactive-form/reactive-form.component.ts
+++ src/app/forms/reactive-form/reactive-form/reactive-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray, AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 // creating custom Validator to check forbidden User names
-
 const forbiddenNamesArray = ['Anna', 'Null'];
 
 function forbiddenNames(control: AbstractControl): { [s: string] : boolean } | null {
@@ -34,7 +34,7 @@ export class ReactiveFormComponent implements OnInit {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl('', [Validators.required, forbiddenNames ]),
-        'email': new FormControl('', [Validators.required, Validators.email])
+        'email': new FormControl('', [Validators.required, Validators.email], this.forbiddenEmail) //async validator
       }),
       'gender': new FormControl('female', Validators.required),
       'hobbies': new FormArray([])
@@ -54,6 +54,19 @@ export class ReactiveFormComponent implements OnInit {
     this.getControls().push(control);
   }
 
+  // Async Custom Validator for email 
 
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise( (resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true})
+        }else {
+          resolve(null);
+        }
+      }, 1500)
+    });
+    return promise;
+  }
 
 }
