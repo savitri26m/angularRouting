5cebf2e2b39fc24542c3343d021022e9fabb6963
Savitri <savitri26m@gmail.com>
2020-06-08 00:49:48 +0530

    added vqlidator

--- src/app/forms/reactive-form/reactive-form/reactive-form.component.ts
+++ src/app/forms/reactive-form/reactive-form/reactive-form.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, AbstractControl } from '@angular/forms';
+
+// creating custom Validator to check forbidden User names
+
+const forbiddenNamesArray = ['Anna', 'Null'];
+
+function forbiddenNames(control: AbstractControl): { [s: string] : boolean } | null {
+  console.log(forbiddenNamesArray.indexOf(control.value))
+  if( forbiddenNamesArray.indexOf(control.value) !== -1) {
+    return {'nameIsForbidden': true}
+  }
+
+  return null;
+}
+
+
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
+
+
 export class ReactiveFormComponent implements OnInit {
   signUpForm: FormGroup;
   genders= ['male', 'female'];
+  
 
   constructor() { }
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl('', Validators.required),
+        'username': new FormControl('', [Validators.required, forbiddenNames ]),
         'email': new FormControl('', [Validators.required, Validators.email])
       }),
       'gender': new FormControl('female', Validators.required),
@@ -35,5 +53,7 @@ export class ReactiveFormComponent implements OnInit {
     const control = new FormControl('', Validators.required);
     this.getControls().push(control);
   }
+
+
 
 }
